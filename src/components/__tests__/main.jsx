0d39619ca95d0508dc4a4628dbe4e23197d44465
estermer <eric.stermer@gmail.com>
2017-11-11 00:18:56 -0500

    added testing tools and set a boiler plate for first test commented out

---
+++ src/components/__tests__/main.jsx
@@ -0,0 +1,16 @@
+// import chai, { expect } from 'chai';
+// import dirtyChai from 'dirty-chai';
+// import React from 'react';
+// import $ from 'teaspoon';
+//
+// import App from '../App';
+//
+// chai.use(dirtyChai);
+//
+// describe('Main App', () => {
+//   it('renders the App container', () => {
+//     const $root = $(<App />).shallowRender();
+//
+//     expect($root.length).to.be.least(1);
+//   });
+// });
